2046c61d95e50b3de4e78f8f626b61ba091293a4
Hypercube <hypercube.softwares.org@gmail.com>
2026-01-26 18:31:14 +0100

    Fix tests

--- midi-workshop/src/test/java/com/hypercube/workshop/midiworkshop/api/presets/MidiPresetTest.java
+++ midi-workshop/src/test/java/com/hypercube/workshop/midiworkshop/api/presets/MidiPresetTest.java
@@ -12,17 +12,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MidiPresetTest {
-    record TestParam(MidiBankFormat midiBankFormat, String input, int expectedBank,
-                     int expectedProgram) {
-    }
-
     private static Stream<TestParam> parseMSBLSBBank() {
         return Stream.of(
-                new TestParam(MidiBankFormat.NO_BANK_PRG, "10", 0, 16),
+                new TestParam(MidiBankFormat.NO_BANK_PRG, "10", 0, 10),
                 new TestParam(MidiBankFormat.BANK_MSB_PRG, "8-10", 0x0008, 10),
+                new TestParam(MidiBankFormat.BANK_MSB_PRG, "080A", 0x0008, 10),
                 new TestParam(MidiBankFormat.BANK_LSB_PRG, "8-108", 0x0008, 108),
-                new TestParam(MidiBankFormat.BANK_MSB_LSB_PRG, "2-8-108", 0x0208, 108)
+                new TestParam(MidiBankFormat.BANK_LSB_PRG, "086C", 0x0008, 108),
+                new TestParam(MidiBankFormat.BANK_MSB_LSB_PRG, "2-8-108", 0x0208, 108),
+                new TestParam(MidiBankFormat.BANK_MSB_LSB_PRG, "02086C", 0x0208, 108)
         );
+    }
+
+    private static Stream<TestParam> parseBrokenMSBLSBBank() {
+        return Stream.of(
+                new TestParam(MidiBankFormat.NO_BANK_PRG, "4-2", 0, 0),
+                new TestParam(MidiBankFormat.BANK_MSB_PRG, "8", 0, 0),
+                new TestParam(MidiBankFormat.BANK_LSB_PRG, "8", 0, 0),
+                new TestParam(MidiBankFormat.BANK_MSB_LSB_PRG, "2-8", 0, 0)
+        );
+    }
+
+    record TestParam(MidiBankFormat midiBankFormat, String input, int expectedBank,
+                     int expectedProgram) {
     }
 
     @ParameterizedTest
@@ -34,15 +46,6 @@ class MidiPresetTest {
         assertEquals(1, preset.getZeroBasedChannel());
         assertEquals(testParam.expectedProgram(), preset.getLastProgram());
         assertEquals(testParam.expectedBank(), preset.getBank());
-    }
-
-    private static Stream<TestParam> parseBrokenMSBLSBBank() {
-        return Stream.of(
-                new TestParam(MidiBankFormat.NO_BANK_PRG, "4-2", 0, 0),
-                new TestParam(MidiBankFormat.BANK_MSB_PRG, "8", 0, 0),
-                new TestParam(MidiBankFormat.BANK_LSB_PRG, "8", 0, 0),
-                new TestParam(MidiBankFormat.BANK_MSB_LSB_PRG, "2-8", 0, 0)
-        );
     }
 
     @ParameterizedTest
