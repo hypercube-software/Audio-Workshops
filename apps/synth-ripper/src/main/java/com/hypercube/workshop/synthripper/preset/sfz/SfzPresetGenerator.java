b48eb262a6658b8c5864c0933454a2383485232e
Hypercube <hypercube.softwares.org@gmail.com>
2026-08-05 21:37:44 +0200

    Fix SfzPresetGenerator

--- apps/synth-ripper/src/main/java/com/hypercube/workshop/synthripper/preset/sfz/SfzPresetGenerator.java
+++ apps/synth-ripper/src/main/java/com/hypercube/workshop/synthripper/preset/sfz/SfzPresetGenerator.java
@@ -1,8 +1,8 @@
 package com.hypercube.workshop.synthripper.preset.sfz;
 
 import com.hypercube.workshop.midiworkshop.api.presets.MidiPreset;
+import com.hypercube.workshop.synthripper.model.MidiZone;
 import com.hypercube.workshop.synthripper.model.RecordedSynthNote;
-import com.hypercube.workshop.synthripper.model.config.MidiSettings;
 import com.hypercube.workshop.synthripper.model.config.SynthRipperConfiguration;
 import com.hypercube.workshop.synthripper.preset.PresetGenerator;
 import org.springframework.stereotype.Component;
@@ -14,6 +14,8 @@ import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.groupingBy;
+
 @Component
 public class SfzPresetGenerator implements PresetGenerator {
     @Override
@@ -23,74 +25,91 @@ public class SfzPresetGenerator implements PresetGenerator {
 
     @Override
     public void generate(SynthRipperConfiguration conf, List<RecordedSynthNote> sampleBatch) {
-        sampleBatch.forEach(sample -> {
-            MidiPreset preset = sample.getPreset();
-            File sfzFile = new File("%s/%s %s.sfz".formatted(conf.getOutputDir(), preset.getFirstProgram(), preset.getId()
+        var recordsPerPresets = sampleBatch.stream()
+                .collect(groupingBy(RecordedSynthNote::getPreset));
+        recordsPerPresets.forEach((preset, recordedSamples) -> {
+            String presetId = preset.getShortId();
+            File sfzFile = new File("%s/%s %s.sfz".formatted(conf.getOutputDir(), presetId, preset.getId()
                     .name()));
             try (PrintWriter out = new PrintWriter(new FileOutputStream(sfzFile))) {
-                final String SEPARATOR = "----------------------------------------------------------";
-                out.println(SEPARATOR);
-                out.println("%s".formatted(preset.getId()));
-                out.println(SEPARATOR);
+                out.println("// %s".formatted(preset.getId()));
                 out.println("<control>");
-                out.println("default_path=.");
+                out.println("default_path=./");
                 out.println("<global>");
-                int prevVelo = -1;
-                int nbVelocityPerNote = conf.getMidi()
-                        .getVelocityPerNote();
-                int velocityOffset = (int) Math.ceil(127.f / nbVelocityPerNote);
-                for (int group = 1; group <= nbVelocityPerNote; group++) {
-                    int startVelo = prevVelo + 1;
-                    int endVelo = Math.min(127, velocityOffset * group);
-                    out.println(SEPARATOR);
-                    out.println(" Velocity layer %03d".formatted(endVelo));
-                    out.println(SEPARATOR);
-                    out.println("<group>");
-                    out.println("lovel=" + startVelo);
-                    out.println("hivel=" + endVelo);
-                    prevVelo = endVelo;
+                out.println();
 
-                    out.println("");
-                    MidiSettings midiSettings = conf.getMidi();
-                    int lowestNote = midiSettings.getLowestNoteInt();
-                    int highestNote = midiSettings.getHighestNoteInt();
-                    int noteIncrement = 12 / midiSettings.getNotesPerOctave();
-                    int nbRegion = Math.min(1, highestNote + 1 - lowestNote / noteIncrement);
-                    for (int r = 0; r < nbRegion; r++) {
-                        out.println("<region>");
-                        int note = lowestNote + r * noteIncrement;
-                        int begin = note;
-                        int end = note + noteIncrement - 1;
-                        if (r == 0) {
-                            begin = 0;
+                var recordsPerVelocity = recordedSamples.stream()
+                        .collect(groupingBy(RecordedSynthNote::getVelocity));
+                recordsPerVelocity.forEach((velocity, recordedSamplesPerVelocity) -> {
+                    var recordsPerCC = recordedSamplesPerVelocity.stream()
+                            .collect(groupingBy(RecordedSynthNote::getControlChange));
+                    recordsPerCC.forEach((cc, recordedSamplesPerControlChange) -> {
+                        if (cc == MidiPreset.NO_CC) {
+                            writeGroup(out, sfzFile, velocity, recordedSamplesPerControlChange);
+                        } else {
+                            var recordsPerCCValue = recordedSamplesPerControlChange.stream()
+                                    .collect(groupingBy(RecordedSynthNote::getCcValue));
+                            recordsPerCCValue.forEach((ccValue, recordedSamplesPerCCValue) -> {
+                                writeCCGroup(out, sfzFile, velocity, cc, ccValue, recordedSamplesPerCCValue);
+                            });
                         }
-                        if (r == nbRegion - 1) {
-                            end = 127;
-                        }
-                        String path = sfzFile.getParentFile()
-                                .toPath()
-                                .relativize(sample.getFile()
-                                        .toPath())
-                                .toString()
-                                .replace("\\", "/");
-
-                        Optional.ofNullable(sample.getLoopSetting())
-                                .ifPresentOrElse(l -> {
-                                    out.println("loop_mode=loop_sustain");
-                                    out.println("loop_start=" + l.getSampleStart());
-                                    out.println("loop_end=" + l.getSampleEnd());
-                                }, () -> out.println("loop_mode=no_loop"));
-
-                        out.println("sample=" + path);
-                        out.println("lokey=" + begin);
-                        out.println("pitch_keycenter=" + note);
-                        out.println("hikey=" + end);
-                        out.println("");
-                    }
-                }
+                    });
+                });
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private void writeGroup(PrintWriter out, File sfzFile, MidiZone velocity, List<RecordedSynthNote> recordedSamples) {
+        out.println("<group>");
+        out.println("lovel=%d".formatted(velocity.low()));
+        out.println("hivel=%d".formatted(velocity.high()));
+        out.println();
+
+        for (RecordedSynthNote sample : recordedSamples) {
+            writeRegion(out, sfzFile, sample);
+        }
+    }
+
+    private void writeCCGroup(PrintWriter out, File sfzFile, MidiZone velocity, int cc, MidiZone ccZone, List<RecordedSynthNote> recordedSamples) {
+        out.println("<group>");
+        out.println("lovel=%d".formatted(velocity.low()));
+        out.println("hivel=%d".formatted(velocity.high()));
+        out.println("loCC%d=%d".formatted(cc, ccZone.low()));
+        out.println("hiCC%d=%d".formatted(cc, ccZone.high()));
+        out.println();
+
+        for (RecordedSynthNote sample : recordedSamples) {
+            writeRegion(out, sfzFile, sample);
+        }
+    }
+
+    private void writeRegion(PrintWriter out, File sfzFile, RecordedSynthNote sample) {
+        out.println("<region>");
+
+        String path = sfzFile.getParentFile()
+                .toPath()
+                .relativize(sample.getFile()
+                        .toPath())
+                .toString()
+                .replace("\\", "/");
+        out.println("sample=" + path);
+
+        MidiZone note = sample.getNote();
+        out.println("lokey=%d".formatted(note.low()));
+        out.println("pitch_keycenter=%d".formatted(note.value()));
+        out.println("hikey=%d".formatted(note.high()));
+
+        out.println("ampeg_release=%.3f".formatted(sample.getReleaseTimeInSec()));
+
+        Optional.ofNullable(sample.getLoopSetting())
+                .ifPresentOrElse(l -> {
+                    out.println("loop_mode=loop_sustain");
+                    out.println("loop_start=" + l.getSampleStart());
+                    out.println("loop_end=" + l.getSampleEnd());
+                }, () -> out.println("loop_mode=no_loop"));
+
+        out.println();
     }
 }
